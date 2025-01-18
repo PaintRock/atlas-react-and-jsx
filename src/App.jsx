c2a8d4d2985ee88e53d24ebb2d5b3c1734065403
Adel Knode <ann.knode@atlasschool.com>
2025-01-18 17:56:07 +0000

    in pprogress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "./components/Header";
-// import Header from "./components/Header";
+import Section from './components/Section';
 
 export default function App() {
   return( <div className="app">
 
     <Header />
-    
+    <Section title="What is React?">
+      <p>React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called "components".</p>
+    </Section>
+
+    <Section title="Benefits of React?">
+      <ul>
+        <li>Declarative</li>
+        <li>Component-Based</li>
+        <li>Learn Once, Write Anywhere</li>
+      </ul>
+    </Section>
     </div>
   );
 };
---
+++ src/components/Section.jsx
@@ -0,0 +1,9 @@
+export default function Section ({ title, children}) {
+    return (
+        <div className = "section">
+        <h1>{title}</h1>
+        {children}
+        </div>
+
+    );
+};
